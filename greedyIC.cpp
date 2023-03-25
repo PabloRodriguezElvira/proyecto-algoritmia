9ff3f0f5414f32e4c9cede5d52f8cf762b84ca22
PabloRodriguezElvira <pablo.rodriguez.elvira@estudiantat.upc.edu>
2023-03-25 14:17:02 +0100

    C++ tiene cosas muy clonker, como los tipos que devuelven las funciones size().

--- greedyIC.cpp
+++ greedyIC.cpp
@@ -13,6 +13,71 @@ using Grafo = vector<VI>;
 using VB = vector<bool>;
 using QueueInt = queue<int>;
 
+
+//Función que visita cada nodo adyacente a u y los activa con una probabilidad prob (condición del modelo IC). 
+//Si activamos un nodo, lo encolamos para realizar el mismo proceso partiendo de él.
+void influenciarNodos(const Grafo& G, VB& activados, VI& C, double prob, int u, int* t, QueueInt& Q) {
+    int pp = prob*10;
+    for (int v : G[u]) {
+        int random = rand()%9;
+        if (!activados[v] && random < pp) {
+            activados[v] = true; 
+            Q.push(v);
+        }
+    }
+    *t = *t + 1;
+}
+
+
+//La función se encarga de para cada vértice activado inicialmente, ir activando sus vecinos según la probabilidad prob.
+VI difusionIC(const Grafo& G, VB& activados, double ratio, QueueInt& Q, int* t) {
+    VI C;
+    while (not Q.empty()) {
+        int w = Q.front(); Q.pop();
+        C.push_back(w);
+        influenciarNodos(G, activados, C, ratio, w, t, Q);
+    } 
+    return C;
+}
+
+// Función para seleccionar el mínimo subconjunto Q que puede activar la mayor cantidad de nodos
+VI seleccionarSubconjunto(const Grafo& G, VB& activados, double prob) {
+    int n = G.size();
+    vector<pair<int, int>> nodos(n);
+
+    for (int i = 0; i < n; i++) {
+        nodos[i].first = G[i].size();
+        nodos[i].second = i;
+    }
+
+    // Ordena los nodos según su grado (en orden descendente)
+    sort(nodos.begin(), nodos.end(), greater<pair<int, int>>());
+
+    // Realiza la influencia desde cada nodo ordenado y elige el que activa la mayor cantidad de nodos
+    int maxActivados = -1;
+    VI subconjunto;
+    for (int i = 0; i < n; i++) {
+        int w = nodos[i].second;
+
+        // Activa el nodo y realizar la difusión
+        VB subactivados(n, false);
+        subactivados[w] = true;
+        QueueInt Q;
+        Q.push(w);
+        int t = 0;
+        VI C = difusionIC(G, subactivados, prob, Q, &t);
+        int size = C.size();
+        // Verifica si este subconjunto activa más nodos que el anterior máximo
+        if (size > maxActivados) {
+            maxActivados = size;
+            activados = subactivados;  // Actualizar el vector de nodos activados
+            subconjunto = C;  // Actualizar el subconjunto C
+        }
+    }
+    return subconjunto;
+}
+
+
 //Función que se encarga de leer el grafo y la probabilidad 
 Grafo leerGrafo(double* prob, QueueInt& Q, VB& activados) {
     //Fichero que contiene el grafo:
@@ -39,77 +104,10 @@ Grafo leerGrafo(double* prob, QueueInt& Q, VB& activados) {
     //Probabilidad:
     getline(file, line); *prob = atof(line.c_str());
     
-    //Subconjunto Q:
-    Q = seleccionarSubconjunto(G, activados, prob);
-
     file.close();
     return G;
 }
 
-// Función para seleccionar el mínimo subconjunto Q que puede activar la mayor cantidad de nodos
-QueueInt seleccionarSubconjunto(const Grafo& G, VB& activados, double prob) {
-    int n = G.size();
-    vector<pair<int, int>> nodos(n);
-
-    for (int i = 0; i < n; i++) {
-        nodos[i].first = G[i].size();
-        nodos[i].second = i;
-    }
-
-    // Ordena los nodos según su grado (en orden descendente)
-    sort(nodos.begin(), nodos.end(), greater<pair<int, int>>());
-
-    // Realiza la influencia desde cada nodo ordenado y elige el que activa la mayor cantidad de nodos
-    int maxActivados = -1;
-    QueueInt subconjunto;
-    for (int i = 0; i < n; i++) {
-        int w = nodos[i].second;
-
-        // Activa el nodo y realizar la difusión
-        VB subactivados(n, false);
-        subactivados[w] = true;
-        QueueInt Q;
-        Q.push(w);
-        int t = 0;
-        VI C = difusionIC(G, subactivados, prob, Q, &t);
-
-        // Verifica si este subconjunto activa más nodos que el anterior máximo
-        if (C.size() > maxActivados) {
-            maxActivados = C.size();
-            activados = subactivados;  // Actualizar el vector de nodos activados
-            subconjunto = Q;  // Actualizar el subconjunto Q
-        }
-    }
-
-    return subconjunto;
-}
-
-//Función que visita cada nodo adyacente a u y los activa con una probabilidad prob (condición del modelo IC). 
-//Si activamos un nodo, lo encolamos para realizar el mismo proceso partiendo de él.
-void influirNodos(const Grafo& G, VB& activados, VI& C, double prob, int u, int* t, QueueInt& Q) {
-    bool influenciado = false;
-    int pp = prob*10;
-    for (int v : G[u]) {
-        int random = rand()%9;
-        if (!activados[v] && random < pp) {
-            influenciado = true;
-            activados[v] = true; 
-            Q.push(v);
-        }
-    }
-    if (influenciado) *t = *t + 1;
-}
-
-//La función se encarga de para cada vértice activado inicialmente, ir activando sus vecinos según la probabilidad prob.
-VI difusionIC(const Grafo& G, VB& activados, double ratio, QueueInt& Q, int* t) {
-    VI C;
-    while (not Q.empty()) {
-        int w = Q.front(); Q.pop();
-        C.push_back(w);
-        influirNodos(G, activados, C, ratio, w, t, Q);
-    } 
-    return C;
-}
 
 int main () {
     //Semilla de los números aleatorios.
@@ -122,7 +120,7 @@ int main () {
     Grafo G = leerGrafo(&prob, Q, activados);
 	
     int t = 0;
-    VI C = difusionIC(G, activados, prob, Q, &t);
+    VI C = seleccionarSubconjunto(G, activados, prob);
 
     //Mostrar respuesta:
     int size = C.size();
